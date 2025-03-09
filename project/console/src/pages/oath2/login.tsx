71ba13a17b5ea660a18c5ea718e9d83efff81cca
ickeep <i@ickeep.com>
2025-03-09 22:07:18 +0800

    feat: 构造跳转过来的请求

--- project/console/src/pages/oath2/login.tsx
+++ project/console/src/pages/oath2/login.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 // 构建跳转过来的请求
-// https://console.home.ickeep.com/oauth2/auth?client_id=c118b70de24c448cbc6aaa0a49c60073&redirect_uri=https://xxxx.app.ickeep.com/oauth2/callback&response_type=code&scope=openid&state=1323213123123
+// https://console.home.ickeep.com/oauth2/auth?client_id=372b719411564344be1d77ab59aad40d&redirect_uri=https://xxxx.app.ickeep.com/oauth2/callback&response_type=code&scope=openid&state=1323213123123
 
 export const Oauth2LoginPage = observer(() => {
   const { search } = useLocation();
