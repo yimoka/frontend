ab72d5d0dd4e005dfb419a98a3b98dea10e9682f
ickeep <i@ickeep.com>
2025-04-14 17:52:30 +0800

    feat: manualChunks

--- project/platform/vite.config.ts
+++ project/platform/vite.config.ts
@@ -34,10 +34,10 @@ export default defineConfig({
         manualChunks: {
           vendor: ['dayjs', 'dayjs/locale/zh-cn', 'axios', 'lodash-es'],
           react: ['react', 'react-dom', 'react-is', 'react-router-dom', 'react/jsx-runtime'],
-          antd: ['antd', 'antd/lib/locale/zh_CN'],
           icon: ['@ant-design/icons'],
           formily: ['@formily/core', '@formily/react', '@formily/reactive'],
-          yimoka: ['@yimoka/shared', '@yimoka/store', '@yimoka/react', '@yimoka/antd'],
+          yimoka: ['@yimoka/shared', '@yimoka/store', '@yimoka/react'],
+          yimokaAntd: ['@yimoka/antd'],
         },
       },
     },
