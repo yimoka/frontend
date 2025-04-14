0fa8e62f4cf6b3a80bd0590d065ca871cfa14e52
ickeep <i@ickeep.com>
2025-04-14 21:04:57 +0800

    feat: Collapse PropsWithComponentData

--- packages/antd/src/display/collapse.tsx
+++ packages/antd/src/display/collapse.tsx
@@ -1,13 +1,12 @@
 import { observer, PropsWithComponentData, useComponentData, useSchemaItemsToItems } from '@yimoka/react';
-import { IAny } from '@yimoka/shared';
 import { Collapse as AntCollapse, CollapseProps as AntCollapseProps } from 'antd';
 import React, { useMemo } from 'react';
 
 const propsMap = { label: 'title' };
 
 export const Collapse = observer((props: CollapseProps) => {
-  const { items, value, data, dataKey, store, ...rest } = props;
-  const curData = useComponentData([data, value], dataKey, store);
+  const { items, data, dataKey, store, ...rest } = props;
+  const curData = useComponentData([data], dataKey, store);
   const schemaItems = useSchemaItemsToItems(curData, propsMap);
   const curItems = useMemo(() => [...(items ?? []), ...(schemaItems ?? [])], [items, schemaItems]);
 
@@ -17,5 +16,5 @@ export const Collapse = observer((props: CollapseProps) => {
 });
 
 
-export type CollapseProps = PropsWithComponentData<AntCollapseProps> & { value?: IAny }
+export type CollapseProps = PropsWithComponentData<AntCollapseProps>
 
