5ec61db67932758ab2825f3158aff14531e52fd8
ickeep <i@ickeep.com>
2025-04-15 00:47:30 +0800

    feat: 移除多余的 console.log

--- packages/antd/src/enter/form.tsx
+++ packages/antd/src/enter/form.tsx
@@ -9,7 +9,6 @@ const { useToken } = theme;
 export function Form<T = IAny>(props: FormProps<T>) {
   const { row, col, labelWidth, labelAttached, ...rest } = props;
   const token = useToken();
-  console.log('rest', rest);
 
   return (
     <FormLayoutContext.Provider value={{ row, col, labelWidth, labelAttached }}>
