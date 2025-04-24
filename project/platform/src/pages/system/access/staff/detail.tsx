7d1cbe24d67a0ebe109565fc5d05f8c446c4d7bf
ickeep <i@ickeep.com>
2025-04-25 01:23:21 +0800

    feat: 人员 配置 添加 编辑 详情

---
+++ project/platform/src/pages/system/access/staff/detail.tsx
@@ -0,0 +1,65 @@
+
+
+import { Modal, ModalProps } from '@yimoka/antd';
+import { EntityDetail, IEntityDetailProps, observer, useExpressionScope } from '@yimoka/react';
+
+import { IAnyObject } from '@yimoka/shared';
+import React from 'react';
+
+import { staffConfig } from './conf';
+
+export const StaffDetailPage = observer((props: Omit<IEntityDetailProps, 'config' | 'schema'>) => (
+  <EntityDetail
+    {...props}
+    config={staffConfig}
+    schema={{
+      type: 'object',
+      properties: {
+        desc: {
+          type: 'void',
+          'x-component': 'Descriptions',
+          'x-component-props': {
+            column: 1,
+            dataKey: 'values',
+            styles: { label: { width: 60 } },
+          },
+          items: {
+            type: 'void',
+            properties: {
+              id: { $ref: '#/definitions/__output__id' },
+              name: { $ref: '#/definitions/__output__name' },
+              switch: { $ref: '#/definitions/__output__switch' },
+              realName: { $ref: '#/definitions/__output__realName' },
+              phonePrefix: { $ref: '#/definitions/__output__phonePrefix' },
+              phone: { $ref: '#/definitions/__output__phone' },
+              mail: { $ref: '#/definitions/__output__mail' },
+              avatar: { $ref: '#/definitions/__output__avatar' },
+              isChangePassword: { $ref: '#/definitions/__output__isChangePassword' },
+              createTime: { $ref: '#/definitions/__output__createTime' },
+              updateTime: { $ref: '#/definitions/__output__updateTime' },
+            },
+          },
+        },
+      },
+    }}
+  />
+));
+
+export const StaffDetailModal = (props: Omit<ModalProps, 'children'> & { values?: IAnyObject }) => {
+  const { values } = props;
+  const record = useExpressionScope()?.$record;
+
+  return (
+    <Modal
+      destroyOnClose
+      footer={false}
+      maskClosable={false}
+      title='人员详情'
+      trigger={{ size: 'small', type: 'primary', ghost: true }}
+      {...props}
+    >
+      <StaffDetailPage values={values ?? record} />
+    </Modal>
+  );
+};
+
