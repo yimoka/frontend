71157c494f4e3aa3f1b1b40e5e9edde939ff77df
ickeep <i@ickeep.com>
2025-05-01 16:59:49 +0800

    feat: 优化 Avatar 组件

--- packages/antd/src/display/avatar.tsx
+++ packages/antd/src/display/avatar.tsx
@@ -1,15 +1,16 @@
+import { withValueFallback } from '@yimoka/react';
 import { AvatarProps as AntAvatarProps, Avatar as AntAvatar } from 'antd';
 import React from 'react';
 
 import { strToIcon } from '../tools/icon';
 
-const AvatarFC = (props: AvatarProps) => {
+type IProps = AntAvatarProps & { value?: IProps['src'] };
+
+const AvatarFC = (props: IProps) => {
   const { icon, src, value, ...rest } = props;
   return <AntAvatar icon={strToIcon(icon)}  {...rest} src={src ?? value} />;
 };
 
-export const Avatar = Object.assign(AvatarFC, {
+export const Avatar = Object.assign(withValueFallback(AvatarFC), {
   Group: AntAvatar.Group,
 });
-
-export type AvatarProps = AntAvatarProps & { value?: AvatarProps['src'] };
